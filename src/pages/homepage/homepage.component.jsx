0d6d06ebf7e5f554b1f385d3b7518f1bd1d44257
Mckenzie Irwin <irwinmck@gmail.com>
2021-03-03 23:09:29 -0500

    init commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// components
+import Directory from '../../components/directory/directory.component'
+
+// styles
+import './homepage.styles.scss'
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  )
+}
+
+export default HomePage
